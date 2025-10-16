8f1bd1ee36ad5faa05539642982fce01ebc8e36f
LaxBloxBoy2 <anwargtagta@gmail.com>
2025-10-16 02:32:56 +0100

    DEDE

--- apps/web/app/[locale]/models/application/client.tsx
+++ apps/web/app/[locale]/models/application/client.tsx
@@ -451,7 +451,7 @@ export default function ModelApplicationClient() {
                           <button
                             type="button"
                             onClick={() => removePortfolioItem(index)}
-                            className="absolute top-1 right-1 text-red-500 hover:text-red-700"
+                            className="absolute -top-2 -right-2 text-red-500 hover:text-red-700 bg-white rounded-full p-1 shadow-sm"
                           >
                             <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                               <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
